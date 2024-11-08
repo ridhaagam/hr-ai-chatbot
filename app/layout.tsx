bc9b34fbd3d9de0bfb6a408cac31a8cbbcc81be4
ridhaagam <muhridhaagam@gmail.com>
2024-11-08 15:09:32 +0700

    edit layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import './globals.css';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  title: 'HR-GenAI',
+  description: 'All about HR and Indonesia Laws.',
 };
 
 export const viewport = {
