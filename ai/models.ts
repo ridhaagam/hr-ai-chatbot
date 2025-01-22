e7c9283d3251018c0bf4566f70917d4f5070d541
ridhaagam <muhridhaagam@gmail.com>
2025-01-23 05:23:39 +0700

    update model

--- ai/models.ts
+++ ai/models.ts
@@ -9,23 +9,17 @@ export interface Model {
 
 export const models: Array<Model> = [
   {
-    id: 'ft:gpt-4o-mini-2024-07-18:galih-wasis-wicaksono::AR7H9cQt',
+    // id: 'ft:gpt-4o-mini-2024-07-18:galih-wasis-wicaksono::AR7H9cQt',
+    // label: 'HR Gen-AI',
+    // apiIdentifier: 'ft:gpt-4o-mini-2024-07-18:galih-wasis-wicaksono::AR7H9cQt',
+    // description: 'Specific model for HR',
+    id: 'ft:gpt-4o-mini-2024-07-18:galih-wasis-wicaksono:industrial-relationship:AsUFb2at',
     label: 'HR Gen-AI',
-    apiIdentifier: 'ft:gpt-4o-mini-2024-07-18:galih-wasis-wicaksono::AR7H9cQt',
+    apiIdentifier:
+      'ft:gpt-4o-mini-2024-07-18:galih-wasis-wicaksono:industrial-relationship:AsUFb2at',
     description: 'Specific model for HR',
   },
-  // {
-  //   id: 'gpt-4o',
-  //   label: 'GPT 4o',
-  //   apiIdentifier: 'gpt-4o',
-  //   description: 'For complex, multi-step tasks',
-  // },
-  // {
-  //   id: 'gpt-4o-blocks',
-  //   label: 'GPT 4o with Blocks',
-  //   apiIdentifier: 'gpt-4o',
-  //   description: 'Collaborate with writing',
-  // },
 ] as const;
 
-export const DEFAULT_MODEL_NAME: string = 'ft:gpt-4o-mini-2024-07-18:galih-wasis-wicaksono::AR7H9cQt';
+export const DEFAULT_MODEL_NAME: string =
+  'ft:gpt-4o-mini-2024-07-18:galih-wasis-wicaksono:industrial-relationship:AsUFb2at';
